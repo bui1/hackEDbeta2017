6e68ebdb5e66e14f610ef4ae56bfaf28ae35a461
bui1@ualberta.ca <bui1@ualberta.ca>
2017-11-18 13:57:41 -0700

    Added small stuff to profile page activity, cant get image working

--- app/src/main/java/com/example/android/hiddengems/profile_setup.java
+++ app/src/main/java/com/example/android/hiddengems/profile_setup.java
@@ -2,12 +2,17 @@ package com.example.android.hiddengems;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
 
 public class profile_setup extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_setup);
     }
+
+
 }
