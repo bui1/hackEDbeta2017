1fc5b8eadd03305ec5bbf7f750d6400a5ce8f513
bui1@ualberta.ca <bui1@ualberta.ca>
2017-11-18 14:48:10 -0700

    added third screen for listing profile locations

---
+++ app/src/main/java/com/example/android/hiddengems/list_screen.java
@@ -0,0 +1,19 @@
+package com.example.android.hiddengems;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class list_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_screen);
+    }
+
+
+
+}
--- app/src/main/java/com/example/android/hiddengems/profile_setup.java
+++ app/src/main/java/com/example/android/hiddengems/profile_setup.java
@@ -1,7 +1,10 @@
 package com.example.android.hiddengems;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 
@@ -12,6 +15,11 @@ public class profile_setup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_setup);
+    }
+
+    public void goToMain(View v){
+        Button toMenuOption = (Button) findViewById(R.id.toMain);
+        startActivity(new Intent(getApplicationContext(), list_screen.class));
     }
 
 
