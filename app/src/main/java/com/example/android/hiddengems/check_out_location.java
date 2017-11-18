9512e2510bf8261b84a00c9b29ad276fd0aba5ed
bui1@ualberta.ca <bui1@ualberta.ca>
2017-11-18 16:57:49 -0700

    working on profile screen

---
+++ app/src/main/java/com/example/android/hiddengems/check_out_location.java
@@ -0,0 +1,35 @@
+package com.example.android.hiddengems;
+
+/**
+ * Created by MonicaB on 2017-11-18.
+ *
+ */
+
+public class check_out_location {
+
+
+
+    public static String[] get_places() {
+        return new String[] {
+                "West Edm Mall",
+                "Startup Edmonton",
+                "Rogers Place",
+                "University of Alberta",
+                "Dream Tea",
+                "Seoul Fried Chicken",
+                "South Edm Common",
+                "End of the World",
+                "Scoop n Roll",
+                "Borden Park",
+                "Northlands Park",
+                "Mercer Tavern",
+                "Computing Science Centre",
+                "City Centre",
+                "Corona Station",
+                "Sugar Bowl",
+                "Remedy's",
+                "Wayne Gretzky Statue"
+
+        };
+    }
+}
--- app/src/main/java/com/example/android/hiddengems/list_screen.java
+++ app/src/main/java/com/example/android/hiddengems/list_screen.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class list_screen extends AppCompatActivity {
 
@@ -12,6 +13,25 @@ public class list_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_screen);
+
+
+        TextView personal_locations = (TextView) findViewById(R.id.list_personal_locations);
+
+        String[] locations = Locations_list.getLocations();
+        for (String location: locations){
+            personal_locations.append(location+"\n\n\n");
+        }
+
+        TextView new_locations = (TextView) findViewById(R.id.wish_list);
+
+        String[] new_places = check_out_location.get_places();
+        for (String place: new_places){
+            new_locations.append(place+"\n\n\n");
+        }
+
+
+
+
     }
 
 
