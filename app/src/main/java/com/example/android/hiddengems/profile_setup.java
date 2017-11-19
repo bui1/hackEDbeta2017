f56e6d43fb8454edd17e3c1e1f5b3c84431236d2
Sundus Mahdi <sundus@ualberta.ca>
2017-11-18 22:53:49 -0700

    fixed setup layout and added textedit stings to profile_setup.java

--- app/src/main/java/com/example/android/hiddengems/profile_setup.java
+++ app/src/main/java/com/example/android/hiddengems/profile_setup.java
@@ -8,9 +8,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import java.io.IOException;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 
 public class profile_setup extends AppCompatActivity {
@@ -21,9 +24,6 @@ public class profile_setup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_setup);
 
-
-
-
     }
 
     public void onClick(View v){
@@ -33,6 +33,17 @@ public class profile_setup extends AppCompatActivity {
         intent.setAction(Intent.ACTION_GET_CONTENT);
 // Always show the chooser (if there are multiple options available)
         startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1);
+    }
+
+    public void sendMessage(View v) {
+        Intent intent = new Intent(this, list_screen.class);
+        EditText editName = (EditText) findViewById(R.id.fullName);
+        EditText editCity = (EditText) findViewById(R.id.cityInput);
+        String messageName = editName.getText().toString();
+        String messageCity = editCity.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, messageName);
+        intent.putExtra(EXTRA_MESSAGE, messageCity);
+        startActivity(intent);
     }
 
     @Override
@@ -54,28 +65,6 @@ public class profile_setup extends AppCompatActivity {
             }
         }
     }
-
-
-
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
-//
-//            Uri uri = data.getData();
-//
-//            try {
-//                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-//                // Log.d(TAG, String.valueOf(bitmap));
-//
-//                ImageView imageView = (ImageView) findViewById(R.id.imageView);
-//                imageView.setImageBitmap(bitmap);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
 
 
     public void goToMain(View v){
